2652f49efdd7f6f2050e690ff26742825ff9d860
Julio_De_Pro <jkubayi27@gmail.com>
2025-03-24 20:13:45 +0200

    Static page

---
+++ resources/script.js
@@ -0,0 +1,20 @@
+//Base code in how to toggle visibility or display of an element
+/* secret-messages.js */
+/*const buttonElement = document.getElementById('secret-button');
+const pElement = document.getElementById('secret-p');
+
+const toggleHiddenElement = (domElement) => {
+    if (domElement.style.display === 'none') {
+      domElement.style.display = 'block';
+    } else {
+      domElement.style.display = 'none';
+    }
+}
+
+buttonElement.addEventListener('click', () => {
+  toggleHiddenElement(pElement);
+});*/
+function hideLanguages() {
+  const lang = document.getElementById('languages');
+  lang.style.display = 'none';
+}
